741d4ff593daf1d30b7b402cabd84f5dcaf633c1
Jennifer <jenndestes@gmail.com>
2021-05-12 14:21:29 -0500

    commit tree 5e731af3c50c404dbcb49dea5f7fb87311abf209

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"];
+
+function writeCards(names) {
+    const arrayOfMessages = []
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+        arrayOfMessages.push(message)
+        console.log(message)
+    }
+    return arrayOfMessages;
+}
+
+function countDown() {
+    let i = 0;
+    while (i < 11) {
+        console.log(i);
+        i++;
+    }
+}
+
+
+
